b4e52eaad6c439e60faef2757523f7d32e391959
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-16 21:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length=== 0){
     return 'There is nobody waiting to be served!';
   }
+  console.log(katzDeliLine);
       katzDeliLine.shift();
       console.log(katzDeliLine);
     return `Currently serving ${katzDeliLine[0]}.`;
